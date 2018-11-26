6c3d35a137cad1dc5e2c2ad0790046e22d54c82a
Unknown <alix.m.leroy@cranfield.ac.uk>
2018-11-26 16:16:43 +0000

    Add filepaths

---
+++ flags/filepath.py
@@ -0,0 +1,43 @@
+
+MAIN = "/home/data/haystack/"
+
+SC1  ="scenario1/"
+SC2 = "scenario2/"
+SC3 = "scenario3/"
+SC4 = "scenario4/"
+SC5 = "scenario5/"
+
+SC1_DEVICE = MAIN + SC1 + "haystack_device.csv"
+SC1_PSYCHO = MAIN + SC1 + "haystack_psychometric.csv"
+SC1_HTTP = MAIN + SC1 + "haystack_http.csv"
+SC1_EMAIL= MAIN + SC1 + "haystack_email.csv"
+SC1_LOGON = MAIN + SC1 + "haystack_logon.csv"
+SC1_FILE = MAIN + SC1 + "haystack_file.csv"
+
+SC2_DEVICE = MAIN + SC2 + "haystack_device.csv"
+SC2_PSYCHO = MAIN + SC2 + "haystack_psychometric.csv"
+SC2_HTTP = MAIN + SC2 + "haystack_http.csv"
+SC2_EMAIL= MAIN + SC2 + "haystack_email.csv"
+SC2_LOGON = MAIN + SC2 + "haystack_logon.csv"
+SC2_FILE = MAIN + SC2 + "haystack_file.csv"
+
+SC3_DEVICE = MAIN + SC3 + "haystack_device.csv"
+SC3_PSYCHO = MAIN + SC3 + "haystack_psychometric.csv"
+SC3_HTTP = MAIN + SC3 + "haystack_http.csv"
+SC3_EMAIL= MAIN + SC3 + "haystack_email.csv"
+SC3_LOGON = MAIN + SC3 + "haystack_logon.csv"
+SC3_FILE = MAIN + SC3 + "haystack_file.csv"
+
+SC4_DEVICE = MAIN + SC4 + "haystack_device.csv"
+SC4_PSYCHO = MAIN + SC4 + "haystack_psychometric.csv"
+SC4_HTTP = MAIN + SC4 + "haystack_http.csv"
+SC4_EMAIL= MAIN + SC4 + "haystack_email.csv"
+SC4_LOGON = MAIN + SC4 + "haystack_logon.csv"
+SC4_FILE = MAIN + SC4 + "haystack_file.csv"
+
+SC5_DEVICE = MAIN + SC5 + "haystack_device.csv"
+SC5_PSYCHO = MAIN + SC5 + "haystack_psychometric.csv"
+SC5_HTTP = MAIN + SC5 + "haystack_http.csv"
+SC5_EMAIL= MAIN + SC5 + "haystack_email.csv"
+SC5_LOGON = MAIN + SC5 + "haystack_logon.csv"
+SC5_FILE = MAIN + SC5 + "haystack_file.csv"
