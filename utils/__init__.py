b77014d8e3995332ccf92ce5f9297f5cd2e19535
samuelwestlake <samuelwestlake@hotmial.co.uk>
2018-11-26 22:40:28 +0000

    features

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,7 @@
-import pandas
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 
 def get_unique_values(file_paths, col_name, **kwargs):
@@ -8,38 +11,50 @@ def get_unique_values(file_paths, col_name, **kwargs):
     :param kwargs: any kwargs for pandas.DataFrame() e.g. sep=","
     :return: numpy.ndarray
     """
-    df = pandas.DataFrame(columns=[col_name])
+    df = pd.DataFrame(columns=[col_name])
     for path in file_paths:
-        tmp = pandas.read_csv(path, **kwargs)
+        tmp = pd.read_csv(path, **kwargs)
         df = df.append(tmp, ignore_index=True, sort=False)
     return df[col_name].unique()
 
 
-def get_activities(file_path, columns=["activity", "pc", "date"]):
+def get_email(file_path):
     """
-    :param file_path: str: path to the file
-    :param columns: list of str: headers to be included in activities
-    :return: dict: data
+    :param file_path:
+    :return:
     """
-    df = pandas.read_csv(file_path)
-    df = df.head()
-    # Initialise the dictionary
-    data = {}
-    # Iterate through the rows
+    pattern = '%m/%d/%Y %H:%M:%S'
+    columns = ["epoch", "freq"]
+    df = pd.read_csv(file_path)
+    df = df.where((pd.notnull(df)), None)
+    df = df[df.activity == "Send"]                      # Only keep emails which are sent (remove view and receive)
+    users = df.user.unique()                            # Get a list of unique user names
+    # Set up data dict
+    data = {user: pd.DataFrame(columns=columns) for user in users}
+    # Get frequency of each email address
+    freq = {}
     for _, row in df.iterrows():
-        user = row["user"]
-        # If user is not in data, add user to data
-        if user not in data:
-            data.update({user: {"activities": []}})
-        # For each item in columns, get a dictionary of the data for the user
-        d = {item: row[item] for item in columns}
-        # Append the data to the user activities
-        data[user]["activities"].append(d)
+        for address in row.to.split(";"):
+            if address not in freq:
+                freq.update({address: 1})
+            else:
+                freq[address] += 1
+    n = sum([value for value in freq.values()])
+    for _, row in df.iterrows():
+        f = np.asarray([freq[address] for address in row.to.split(";")])
+        f = np.max(- np.log(f / n))
+        epoch = int(time.mktime(time.strptime(row.date, pattern)))
+        tmp = pd.DataFrame([[epoch, f]], columns=columns)
+        data[row.user] = data[row.user].append(tmp)
     return data
 
 
 if __name__ == "__main__":
-    # print(get_unique_values(["data/1.csv", "data/2.csv"], "user_id", sep=","))
-    print(get_activities("../data/haystack_file.csv"))
+    data = get_email("../data/haystack_email.csv")
+    for user, d in data.items():
+        print(user)
+        print(d)
+        print("-----------")
+
 
 
